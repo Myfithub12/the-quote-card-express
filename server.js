ae3469986b7d589eafa15c4544b904fddbfc9e48
Mark Hollrah <mhollrah0001@gmail.com>
2026-01-31 08:40:32 -0500

    NPM and JSON Install

---
+++ server.js
@@ -0,0 +1,16 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+
+const port = 8800;
+
+app.use(express.static("./public"));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+app.listen(port, () => {
+    console.log( 'Server is runnig http://localhost:${port}');
+    console.log("Press Ctrl+C to end this process.");
+})
