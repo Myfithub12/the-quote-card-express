3b94262176f594ccc8cf69b7192f800d3eb0c4a3
Mark Hollrah <mhollrah0001@gmail.com>
2026-02-09 19:23:43 -0500

    API

--- public/js/index.js
+++ public/js/index.js
@@ -38,17 +38,7 @@ function loopThroughQuotes() {
     }, 3000);
 } */
 
-async function getRandomImage() {
-    const client_id = "YOUR_ACCESS_KEY";
-    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${client_id}`;
-    try {
-        const response = await fetch(endpoint);
-        const returnedData = await response.json()
-        console.log(returnedData)
-    } catch (error) {
-        console.error(error)
-    }
-}
+
 
 getRandomImage();
 
